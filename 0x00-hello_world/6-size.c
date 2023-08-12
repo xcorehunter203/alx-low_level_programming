4b28f03a3e90210acac0b4494dc0e2918d3099aa
xcorehunter203 <smily.hisham@gmail.com>
2023-08-11 17:43:24 -0700

    i am betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("size of an char: %lu byte(s)\n", sizeof(char);
+	printf("size of an char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an long: %lu byte(s)\n", sizeof(long int));
 	printf("Size of an long long: %lu byte(s)\n", sizeof(long long int));
