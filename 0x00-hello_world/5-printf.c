0d3177d2946dfd8404c46ef262eda9727ed97dd0
xcorehunter203 <smily.hisham@gmail.com>
2023-08-11 17:02:13 -0700

    i am betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
