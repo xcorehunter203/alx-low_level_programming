c09871778175de2a902665d7904592ef35adb91a
xcorehunter203 <smily.hisham@gmail.com>
2023-08-14 16:25:32 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+		printf("%i\n", i);
+	return (0);
+}
