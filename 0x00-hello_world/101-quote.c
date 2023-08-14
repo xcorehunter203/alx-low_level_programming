f9a181715489f80d098662f8c13c1100fb849f46
xcorehunter203 <smily.hisham@gmail.com>
2023-08-14 06:57:15 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19" , stdout);
+	return (1);
+}
