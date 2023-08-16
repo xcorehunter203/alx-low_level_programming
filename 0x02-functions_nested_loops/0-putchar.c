2124f6a36b9897e03288944eaa9d14b31157949d
xcorehunter203 <smily.hisham@gmail.com>
2023-08-15 17:54:49 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+	char ch[] = "_putchar";
+
+	for (int i = 0; i <= 8 ; i++)
+		putchar(ch[i]);
+	putchar('\n');
+	return (0);
+}
