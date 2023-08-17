e344ed389a7ca9cf831c94633397ef805e924847
xcorehunter203 <smily.hisham@gmail.com>
2023-08-16 17:18:04 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_sign - print + if > 0, print - if < 0, print 0 if = 0
+*@n: takes type of input
+*Return: 1 if + , 0 and -1 if -
+*/
+int print_sign(int n)
+{
+	if (n >= 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
