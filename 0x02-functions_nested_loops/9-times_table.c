c9d5d326f4360e250faeffb29a03ea5c8f8fa3dd
xcorehunter203 <smily.hisham@gmail.com>
2023-08-16 19:09:36 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,10 @@
 void times_table(void)
 {
 	int i, j, k;
-	_putchar(48);
-	for(i = 0; i <= 9; i++){
+	
+	for(i = 0; i <= 9; i++)
+	{
+		_putchar(48);
 		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
