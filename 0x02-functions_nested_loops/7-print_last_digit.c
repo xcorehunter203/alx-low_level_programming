6e644b9ec64c20746e0ed983baf1cbdca617a8e1
xcorehunter203 <smily.hisham@gmail.com>
2023-08-16 18:25:29 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_last_digit - computes the last digit
+*@n: takes type of input
+*@x: last digit of n
+*Return: always 0
+*/
+int print_last_digit(int n)
+{
+	int digit;
+
+	if (n > 0)
+		digit = -1 * (n % 10);
+	else
+		digit = n % 10;
+	return (digit);
+}
+
