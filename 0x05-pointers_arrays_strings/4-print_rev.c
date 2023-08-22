f272f55006cb65622f0afedd6ae4e203b99ea980
xcorehunter203 <smily.hisham@gmail.com>
2023-08-22 13:17:02 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints revealed string ,followed by a new line
+ * @s: pointer to the string to print
+ * Return: Nothing
+ */
+
+void print_rev(char *s);
+{
+	int i = 0;
+
+	while (i++)
+		s[i];
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
