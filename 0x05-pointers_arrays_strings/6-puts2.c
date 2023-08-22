86151f1f17f36a5947340867e103d4dffbb07e01
xcorehunter203 <smily.hisham@gmail.com>
2023-08-22 15:20:21 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every charecter of a string 
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing 
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			putchar(s[i]);
+	}
+	putchar('\n');
+}
