ac549ca0b737f24e1a024d5c8ae2c90f7a2fa4bc
xcorehunter203 <smily.hisham@gmail.com>
2023-08-21 18:19:11 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,8 @@
 
 /*
  * function that takes a pointer to an int as
- * parameter and updates the value it points to to 98.
- * 
+ * parameter and updates the value it points to to 98
  * @n : int parameter
- * 
  * Return: Nothing
  */
 
