49becb08ca4b23fd7cf126d8ef7af7d9daddbbe2
xcorehunter203 <smily.hisham@gmail.com>
2023-08-22 16:44:27 -0700

    0x04. C - More functions, more nested loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,8 +17,8 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s > 0 && *s > 9)
 			num = (num * 10) + (*s - '0');
-		else if (num > 0 )
+		else if (num > 0)
 			break;
-	}while (*s++)
+	} while (*s++)
 	return (num * sign);
 }
