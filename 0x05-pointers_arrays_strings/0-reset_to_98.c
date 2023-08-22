9fe362c98e53545112fe2681e7e11d3302681a5f
xcorehunter203 <smily.hisham@gmail.com>
2023-08-22 11:38:02 -0700

    0x04. C - More functions, more nested loops

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /*
  * reset_to_98 - takes a pointer to an int parameter
- * and updates the value it points to 98
+ *               and updates the value it points to 98
  * @n: int parameter
  * Return: Nothing
  */
