86cac2c15fbe51b956387df3c8803b34fabb0d8d
xcorehunter203 <smily.hisham@gmail.com>
2023-08-28 18:46:22 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strchr - file memory with a constant byte
+ * @s: pointer to put the constant
+ * @c: constant
+ * Return: pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int p;
+
+	for (p = 0; s[p] >= '\0'; p++)
+	{
+		if (s[p] == c)
+			return (s + p);
+	}
+}
