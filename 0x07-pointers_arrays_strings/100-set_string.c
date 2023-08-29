9618de03479fa114b89afd16a9753428107c150c
xcorehunter203 <smily.hisham@gmail.com>
2023-08-28 19:53:37 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * set_string - redirect.
  * @s: double pointer to address
@@ -7,6 +7,7 @@
  *
  * Return: chessboard.
  */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
