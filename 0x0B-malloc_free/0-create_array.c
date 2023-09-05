0ffdbc7d5faaa13b7d6dee1103adf2dce45fc4a6
xcorehunter203 <smily.hisham@gmail.com>
2023-09-05 16:13:42 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
+
 	if (size == 0 || n == 0)
 		return (0);
 	while (size--)
 	{
 		n[size] = c;
 	}
-	return n;
+	return (n);
 }
